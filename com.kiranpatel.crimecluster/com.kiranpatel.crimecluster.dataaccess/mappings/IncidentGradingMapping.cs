9f5db4d67d61929523908e931136b9a99922b07e
Kiran94 <kiran_patel94@hotmail.com>
2016-10-29 21:58:44 +0100

    Added IncidentGrading entity and mapping

---
+++ com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.dataaccess/mappings/IncidentGradingMapping.cs
@@ -0,0 +1,25 @@
+﻿namespace com.kiranpatel.crimecluster.dataaccess
+{
+	using System;
+	using NHibernate.Mapping.ByCode.Conformist;
+	using com.kiranpatel.crimecluster.framework;
+
+	/// <summary>
+	/// Mapping for the <see cref="IncidentGrading"/> entity
+	/// </summary>
+	public class IncidentGradingMapping : ClassMapping<IncidentGrading>
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:com.kiranpatel.crimecluster.dataaccess.IncidentGradingMapping"/> class.
+		/// </summary>
+		public IncidentGradingMapping() 
+		{
+			this.Table("IncidentGrading");
+
+			this.Id(o => o.ID);
+			this.Property(o => o.GradeValue);
+			this.Property(o => o.Description);
+			this.Property(o => o.IsDeleted); 
+		}
+	}
+}
---
+++ com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.framework/entities/IncidentGrading.cs
@@ -0,0 +1,22 @@
+﻿namespace com.kiranpatel.crimecluster.framework
+{
+	using System;
+
+	/// <summary>
+	/// Represents an IncidentGrading entity
+	/// </summary>
+	public class IncidentGrading : EntityBase
+	{
+		/// <summary>
+		/// Gets or sets the grade value.
+		/// </summary>
+		/// <value>The grade value.</value>
+		public int GradeValue { get; set;}
+
+		/// <summary>
+		/// Gets or sets the description.
+		/// </summary>
+		/// <value>The description.</value>
+		public String Description { get; set; }
+	}
+}
