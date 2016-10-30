b8493230a2a6cb1d886b7a6e38d26296eb40d224
Kiran94 <kiran_patel94@hotmail.com>
2016-10-30 20:02:33 +0000

    Added Serialisation Service

---
+++ com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.framework/contracts/ISerialisationService.cs
@@ -0,0 +1,25 @@
+﻿namespace com.kiranpatel.crimecluster.framework
+{
+	using System;
+	using Newtonsoft.Json;
+
+	/// <summary>
+	/// Contract for the Serialisation Service 
+	/// </summary>
+	public interface ISerialisationService
+	{
+		/// <summary>
+		/// Serialise the specified toSerialise.
+		/// </summary>
+		/// <param name="toSerialise">To serialise.</param>
+		/// <typeparam name="T">The type of object to serialise</typeparam>
+		String serialise<T>(T toSerialise) where T : EntityBase;
+
+		/// <summary>
+		/// Deserialise the specified toDeserialise.
+		/// </summary>
+		/// <param name="toDeserialise">To deserialise.</param>
+		/// <typeparam name="T">The type of object to deserialise</typeparam>
+		T deserialise<T>(String toDeserialise) where T : EntityBase;
+	}
+}
---
+++ com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.framework/services/SerialisationService.cs
@@ -0,0 +1,39 @@
+﻿namespace com.kiranpatel.crimecluster.framework
+{
+	using System;
+	using Newtonsoft.Json; 
+
+	/// <summary>
+	/// Service for Serialisating and Deserialising Objects
+	/// </summary>
+	public class SerialisationService : ISerialisationService
+	{
+		/// <summary>
+		/// The logger instance
+		/// </summary>
+		private readonly ILogger logger; 
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:com.kiranpatel.crimecluster.framework.SerialisationService"/> class.
+		/// </summary>
+		/// <param name="logger">Logger.</param>
+		public SerialisationService(ILogger logger)
+		{
+			this.logger = logger; 
+		}
+
+		// <inheritdoc>
+		public string serialise<T>(T toSerialise) where T : EntityBase
+		{
+			this.logger.info(String.Format("Serialising Object {0} of Type {1}", toSerialise.ID.ToString(), typeof(T).ToString())); 
+			return JsonConvert.SerializeObject(toSerialise); 
+		}
+
+		// <inheritdoc>
+		public T deserialise<T>(string toDeserialise) where T : EntityBase
+		{
+			this.logger.info(String.Format("Deserialising Object to type {0}", typeof(T).ToString()));
+			return JsonConvert.DeserializeObject<T>(toDeserialise); 
+		}
+	}
+}
