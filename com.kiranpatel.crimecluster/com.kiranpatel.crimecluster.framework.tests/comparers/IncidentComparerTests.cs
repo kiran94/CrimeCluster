bf94fbe8a4dcb321766fd9cf5e299553ec39580c
Kiran94 <kiran_patel94@hotmail.com>
2016-10-30 16:52:00 +0000

    Added Comparison logic for Incident objects

---
+++ com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.framework.tests/comparers/IncidentComparerTests.cs
@@ -0,0 +1,66 @@
+﻿namespace com.kiranpatel.crimecluster.framework.tests
+{
+	using System;
+	using NUnit.Framework; 
+
+	/// <summary>
+	/// Tests for the <see cref="IncidentComparer"/> class
+	/// </summary>
+	[TestFixture]
+	public class IncidentComparerTests
+	{
+		/// <summary>
+		/// Ensures when Incident 1 has a higher importance (lower grading) then Incident 2, 
+		/// A one is returned to indicate paramter one has higher importance then two
+		/// </summary>
+		[Test]
+		public void compare_Incident1HigherImportance_Incident1Grading1AndIncident2Grading3_One()
+		{
+			Incident incident1 = new Incident() { Grading = new IncidentGrading() { GradeValue = 1 } };
+			Incident incident2 = new Incident() { Grading = new IncidentGrading() { GradeValue = 3 } };
+
+			int result = this.GetInstance().Compare(incident1, incident2);
+
+			Assert.AreEqual(1, result); 
+		}
+
+		/// <summary>
+		/// Ensures when Incident 2 has higher importance (lower grading) then Incident 1, 
+		/// A Minus One is returned to indicate parameter two has higher importance then one
+		/// </summary>
+		[Test]
+		public void compare_Incident2HigherImportance_Incident1Grading2AndInciden2Grading1_MinusOne()
+		{
+			Incident incident1 = new Incident() { Grading = new IncidentGrading() { GradeValue = 3 } };
+			Incident incident2 = new Incident() { Grading = new IncidentGrading() { GradeValue = 1 } };
+
+			int result = this.GetInstance().Compare(incident1, incident2);
+
+			Assert.AreEqual(-1, result);
+		}
+
+		/// <summary>
+		/// Ensures when both incidents have the same grading, a zero is returned 
+		//  to indicate they have equal importance
+		/// </summary>
+		[Test]
+		public void compare_Incident1And2Equal_Zero()
+		{
+			Incident incident1 = new Incident() { Grading = new IncidentGrading() { GradeValue = 1 } };
+			Incident incident2 = new Incident() { Grading = new IncidentGrading() { GradeValue = 1 } };
+
+			int result = this.GetInstance().Compare(incident1, incident2);
+
+			Assert.AreEqual(0, result);
+		}
+
+		/// <summary>
+		/// Gets the instance.
+		/// </summary>
+		/// <returns>The instance.</returns>
+		public IncidentComparer GetInstance()
+		{
+			return new IncidentComparer(); 
+		}
+	}
+}
---
+++ com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.framework/comparers/IncidentComparer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace com.kiranpatel.crimecluster.framework
+{
+	/// <summary>
+	/// Defines the rules for comparing 2 <see cref="Incident"/> instances
+	/// </summary>
+	public class IncidentComparer : IComparer<Incident>
+	{
+		// <inheritdoc>
+		public int Compare(Incident x, Incident y)
+		{
+			if (x == null || y == null)
+			{
+				return 0; 
+			}
+
+			// x has higher importance then y
+			if (x.Grading.GradeValue < y.Grading.GradeValue)
+			{
+				return 1;
+			}
+
+			// y has greater importance then x
+			if (x.Grading.GradeValue > y.Grading.GradeValue)
+			{
+				return -1;
+			}
+
+			return 0; 
+
+		}
+	}
+}
