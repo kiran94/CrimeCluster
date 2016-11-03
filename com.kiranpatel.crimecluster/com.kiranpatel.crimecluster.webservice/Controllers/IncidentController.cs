3d5f388a145143a3688f98d71dbbaa39f1dc9f69
Kiran94 <kiran_patel94@hotmail.com>
2016-11-03 14:23:00 +0000

    Added method stubs for IncidentController

---
+++ com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.webservice/Controllers/IncidentController.cs
@@ -0,0 +1,93 @@
+﻿namespace com.kiranpatel.crimecluster.webservice.Controllers
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Web;
+	using System.Web.Mvc;
+
+	using com.kiranpatel.crimecluster.framework; 
+
+	/// <summary>
+	/// Incident controller.
+	/// </summary>
+	public class IncidentController : BaseController
+    {
+		/// <summary>
+		/// The incident service.
+		/// </summary>
+		private readonly IncidentService incidentService;
+
+		/// <summary>
+		/// The incident backlog service.
+		/// </summary>
+		private readonly IncidentBacklogService incidentBacklogService; 
+
+		/// <summary>
+		/// Initializes a new instance of the
+		/// <see cref="T:com.kiranpatel.crimecluster.webservice.Controllers.IncidentController"/> class.
+		/// </summary>
+		/// <param name="repository">Repository.</param>
+		/// <param name="configService">Config service.</param>
+		/// <param name="logger">Logger.</param>
+		/// <param name="serialisationService">Serialisation service.</param>
+		/// <param name="incidentService">Incident service.</param>
+		/// <param name="incidentBacklogService">Incident backlog service.</param>
+		public IncidentController(
+			IRepository repository,
+			IConfigurationService configService,
+			ILogger logger,
+			ISerialisationService serialisationService, 
+			IncidentService incidentService, 
+			IncidentBacklogService incidentBacklogService)
+			: base(repository, configService, logger, serialisationService)
+		{
+			this.incidentService = incidentService;
+			this.incidentBacklogService = incidentBacklogService; 
+		}
+
+		/// <summary>
+		/// Save the specified serialisedIncident.
+		/// POST: /Save
+		/// </summary>
+		/// <param name="serialisedIncident">Serialised incident.</param>
+		[HttpPost]
+		public String Save(String serialisedIncident)
+		{
+			throw new NotImplementedException(); 	
+		}
+
+		/// <summary>
+		/// Gets the Incident with the specified ID
+		/// GET: /Get
+		/// </summary>
+		/// <param name="ID">Identifier.</param>
+		[HttpGet]
+		public String Get(String ID)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Update the the passed incident
+		/// POST: /Update
+		/// </summary>
+		/// <param name="serialisedIncident">Serialised incident.</param>
+		[HttpPost]
+		public String Update(String serialisedIncident)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Deletes the passed incident
+		/// POST: /Delete
+		/// </summary>
+		/// <param name="serialisedIncident">Serialised incident.</param>
+		[HttpPost]
+		public String Delete(String serialisedIncident)
+		{
+			throw new NotImplementedException();
+		}
+    }
+}
