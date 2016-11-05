4cd274fd92f9d05440254b76e16d06011e3a5211
Kiran94 <kiran_patel94@hotmail.com>
2016-11-05 18:06:08 +0000

    Added contract for Prediction classes

---
+++ com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.framework/contracts/IPredictionService.cs
@@ -0,0 +1,17 @@
+﻿namespace com.kiranpatel.crimecluster.framework
+{
+	using System;
+	using System.Collections.Generic; 
+
+	/// <summary>
+	/// Contract for the Prediction Service
+	/// </summary>
+	public interface IPredictionService
+	{
+		/// <summary>
+		/// Predicts a list of potential <see cref="Incident"/> locations based on a data set of <see cref="Incident"/> cases
+		/// </summary>
+		/// <param name="dataSet">Data set to make predictions on.</param>
+		ICollection<Incident> predict(ICollection<Incident> dataSet);
+	}
+}
