07b473372c0ba336eb9a3dc17abcb47cc30d806e
Kiran94 <kiran_patel94@hotmail.com>
2016-12-18 13:58:23 +0000

    Added drop down and ajax method to retrieve the locations for a particular crime type

--- com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.webservice/Scripts/GoogleMaps.js
+++ com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.webservice/Scripts/GoogleMaps.js
@@ -2,6 +2,11 @@
 /* Google Maps Variable*/
 var map; 
 
+/* Markers to plot */
+var markers = []; 
+
+var locationsFound = $('#locationsfound'); 
+
 /* Initialises the map instance */
 function initMap()
 {
@@ -12,3 +17,63 @@ function initMap()
 	    zoom: 12
 	});
 }
+
+/* Makes an AJAX call to retrieve all locations associated with the pased crime type  */
+function filterByCrimeType(crimeType)
+{
+	var bound = new google.maps.LatLngBounds(); 
+
+	$.post(
+	{
+		url : "/Cluster/Filter",
+		data: { "crimeType" : crimeType },
+		success: function(data, status, xhr)
+		{
+			var result = $.parseJSON(data); 
+			var locations = 0; 
+
+			for (var i = 0; i < result.length; i++)
+			{
+				var currentLoc = new google.maps.LatLng(result[i].Latitude, result[i].Longitude); 
+				markers.push(new google.maps.Marker(
+				{
+					position : currentLoc,
+					map : map
+				})); 
+
+				bound.extend(currentLoc); 
+				locations++; 
+			}
+
+			map.setCenter(bound.getCenter()); 
+			map.fitBounds(bound); 
+
+			locationsFound.html(locations); 
+		}
+	});
+}
+
+/* Clears the markers currently on the map */
+function clearMarkers()
+{
+	if (markers)
+	{
+		for (var i = 0; i < markers.length; i++)
+		{
+			markers[i].setMap(null); 
+		}
+	}
+
+	markers = [];
+}
+
+/* On change of the drop down, get the selected item and plot the crimes.  */
+$('#type').change(function()
+{
+ 	$(this).prop("disabled", true);
+	var selected = $(this).val(); 
+
+	clearMarkers(); 
+	filterByCrimeType(selected);  
+	$(this).prop("disabled", false);
+});
