e0a213b657f0e85dd1825650946438f0565d6a8b
Kiran94 <kiran_patel94@hotmail.com>
2017-02-11 17:43:07 +0000

    Renamed Markov Model class and implemented Transition Matrix generation

---
+++ com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.framework/contracts/IMarkovModel.cs
@@ -0,0 +1,20 @@
+﻿namespace com.kiranpatel.crimecluster.framework
+{
+	using System;
+	using System.Collections.Generic;
+
+	/// <summary>
+	/// Contract for a Markov Model
+	/// </summary>
+	public interface IMarkovModel : IPredictionService
+	{
+		/// <summary>
+		/// Generates the transition matrix by going through the incidents and marking down the clusters any of the incidents are in and 
+		/// the next cluster they transition into. The frequencies are counted and then probabilites are determined from the total observerations
+		/// </summary>
+		/// <returns>The transition matrix.</returns>
+		/// <param name="incidents">Incidents.</param>
+		/// <param name="clusters">Clusters.</param>
+		double[,] generateTransitionMatrix(ICollection<Incident> incidents, List<Cluster> clusters);
+	}
+}
---
+++ com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.framework/services/PredictionAlgorithms/MarkovModel.cs
@@ -0,0 +1,111 @@
+﻿namespace com.kiranpatel.crimecluster.framework
+{
+	using System;
+	using System.Linq;
+	using System.Collections.Generic;
+
+	/// <summary>
+	/// Hidden markov model implementation.
+	/// </summary>
+	public class MarkovModel : IMarkovModel
+	{
+		/// <summary>
+		/// The transition matrix.
+		/// </summary>
+		private double[,] _transitionMatrix;
+
+		/// <summary>
+		/// The logger.
+		/// </summary>
+		private readonly ILogger logger; 
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:com.kiranpatel.crimecluster.framework.MarkovModel"/> class.
+		/// </summary>
+		/// <param name="logger">Logger.</param>
+		public MarkovModel(ILogger logger)
+		{
+			this.logger = logger; 
+		}
+
+		// <inheritdoc>
+		public double[,] generateTransitionMatrix(ICollection<Incident> incidents, List<Cluster> clusters)
+		{
+			var clustersFound = new Queue<int>();
+
+			foreach (var currentIncident in incidents)
+			{
+				// maybe improve this? O(n^2)
+				foreach (var currentCluster in clusters)
+				{
+					if (currentCluster.Contains(
+						currentIncident.Location.Latitude.Value, 
+						currentIncident.Location.Longitude.Value))
+					{
+						clustersFound.Enqueue(currentCluster.Label);
+						continue; 
+					}
+				}
+			}
+
+			this._transitionMatrix = this.generateTransitionMatrix(clustersFound, clusters.Count);
+			return this._transitionMatrix; 
+		}
+
+		/// <summary>
+		/// Generates a Transition Matrix from the passed queue of cluster labels
+		/// </summary>
+		/// <returns>The transition matrix.</returns>
+		/// <param name="clustersFound">Clusters found.</param>
+		/// <param name="NoClusters">No clusters.</param>
+		private double[,] generateTransitionMatrix(Queue<int> clustersFound, int NoClusters)
+		{			
+			if (clustersFound.Count == 0)
+			{
+				Exception e = new InvalidOperationException();
+				this.logger.error("No Points were found in Clusters", e);
+				throw e;
+			}
+
+			var transitionMatrix = new double[NoClusters, NoClusters];
+			var rowTotals = new double[NoClusters]; 
+
+			int previous = clustersFound.Dequeue();
+			while (clustersFound.Count != 0)
+			{
+				int next = clustersFound.Dequeue();
+				transitionMatrix[previous, next]++;
+				rowTotals[previous]++; 
+
+				previous = next;
+			}
+
+			return this.calcFrequencies(transitionMatrix, rowTotals); 
+		}
+
+		/// <summary>
+		/// Calculates the frequencies of each of the cells in the transition matrix according to the corresponding row total
+		/// </summary>
+		/// <returns>The frequencies.</returns>
+		/// <param name="transitionMatrix">Transition matrix.</param>
+		/// <param name="rowTotals">Row totals.</param>
+		private double[,] calcFrequencies(double[,] transitionMatrix, double[] rowTotals)
+		{
+			for (int i = 0; i < transitionMatrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < transitionMatrix.GetLength(1); j++)
+				{
+					transitionMatrix[i,j] /= rowTotals[i]; 
+				}
+			}
+
+			return transitionMatrix;
+		}
+
+		// <inheritdoc>
+		public ICollection<Incident> predict(ICollection<Incident> dataSet)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
