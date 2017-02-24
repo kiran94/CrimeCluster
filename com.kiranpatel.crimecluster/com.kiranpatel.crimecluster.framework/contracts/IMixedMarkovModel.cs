b471f8371280c40e08956be31ec13365dcb07563
Kiran94 <kiran_patel94@hotmail.com>
2017-02-24 20:09:27 +0000

    Added Mixed Markov Model interface

---
+++ com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.framework/contracts/IMixedMarkovModel.cs
@@ -0,0 +1,21 @@
+﻿namespace com.kiranpatel.crimecluster.framework
+{
+	using System;
+
+	/// <summary>
+	/// Contract for Mixed Markov Model.
+	/// </summary>
+	public interface IMixedMarkovModel
+	{
+		/// <summary>
+		/// Generates the Mixed Markov Model.
+		/// </summary>
+		void GenerateModel();
+
+		/// <summary>
+		/// Predicts the next location for the given type.
+		/// </summary>
+		/// <param name="type">type to predict against.</param>
+		double[] Predict(CrimeType type);
+	}
+}
