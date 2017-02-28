54d329d2bdfc81a657104afd2a700e8a14d10e52
Kiran94 <kiran_patel94@hotmail.com>
2017-02-28 14:56:19 +0000

    Implemented IQueryable extension method: ToHashSet

---
+++ com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.framework/extensions/IQueryableExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace com.kiranpatel.crimecluster.framework
+{
+	using System;
+	using System.Linq;
+	using System.Collections.Generic;
+
+	/// <summary>
+	/// Extensions for IQueryable. 
+	/// </summary>
+	public static class IQueryableExtensions
+	{
+		/// <summary>
+		/// Converts a queryable into a HashSet. 
+		/// </summary>
+		/// <returns>The hash set.</returns>
+		/// <param name="queryable">Queryable.</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static HashSet<T> ToHashSet<T>(this IQueryable<T> queryable)
+		{
+			return new HashSet<T>(queryable); 
+		}
+	}
+}
