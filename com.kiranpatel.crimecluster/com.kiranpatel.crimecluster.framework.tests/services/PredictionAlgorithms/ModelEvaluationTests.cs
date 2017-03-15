353ee55535b69a82da0e4115d14a3cbc8bb43400
Kiran94 <kiran_patel94@hotmail.com>
2017-03-15 15:29:45 +0000

    updated test class with new dependencies

--- com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.framework.tests/services/PredictionAlgorithms/ModelEvaluationTests.cs
+++ com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.framework.tests/services/PredictionAlgorithms/ModelEvaluationTests.cs
@@ -16,9 +16,31 @@
 		private Mock<IMixedMarkovModel> mixedMarkovModel;
 
 		/// <summary>
+		/// The incident service mock.
+		/// </summary>
+		private Mock<IIncidentService> incidentService;
+
+		/// <summary>
+		/// The distance measure mock.
+		/// </summary>
+		private Mock<IDistanceMeasure> distanceMeasure; 
+
+		/// <summary>
 		/// The logger mock.
 		/// </summary>
 		private Mock<ILogger> logger;
+
+		/// <summary>
+		/// Sets up.
+		/// </summary>
+		[SetUp]
+		public void SetUp()
+		{
+			this.mixedMarkovModel = new Mock<IMixedMarkovModel>();
+			this.incidentService = new Mock<IIncidentService>();
+			this.distanceMeasure = new Mock<IDistanceMeasure>(); 
+			this.logger = new Mock<ILogger>(); 
+		}
 
 		/// <summary>
 		/// Ensures in the normal case, the model is evaluated. 
@@ -35,7 +57,11 @@
 		/// <returns>The instance.</returns>
 		private ModelEvaluation GetInstance()
 		{
-			return new ModelEvaluation(this.mixedMarkovModel.Object, this.logger.Object);
+			return new ModelEvaluation(
+				this.mixedMarkovModel.Object, 
+				this.incidentService.Object,  
+				this.distanceMeasure.Object,
+				this.logger.Object);
 		}
 	}
 }
