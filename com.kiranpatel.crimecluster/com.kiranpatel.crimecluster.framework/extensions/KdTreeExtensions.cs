b3cfca34eeebb41ec70e09ad2d88a126b399787c
Kiran94 <kiran_patel94@hotmail.com>
2017-03-16 16:37:21 +0000

    Implemented KdTree Average Extension

---
+++ com.kiranpatel.crimecluster/com.kiranpatel.crimecluster.framework/extensions/KdTreeExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace com.kiranpatel.crimecluster.framework
+{
+	using System;
+	using KdTree;
+
+	/// <summary>
+	/// Extensions to the KD Tree implementation. 
+	/// </summary>
+	public static class KdTreeExtensions
+	{
+		/// <summary>
+		/// Computes the average latitude and longitude for a given kd-tree.
+		/// </summary>
+		/// <param name="value">Value.</param>
+		public static double[] Average(this KdTree<double, string> value)
+		{
+			var enumerator = value.GetEnumerator();
+			double latitude = 0;
+			double longitude = 0; 
+
+			while (enumerator.MoveNext())
+			{
+				double[] current = enumerator.Current.Point;
+				latitude += current[0];
+				longitude += current[1];
+			}
+
+			latitude /= value.Count;
+			longitude /= value.Count;
+
+			return new double[] { latitude*2, longitude*2 };
+		}
+	}
+}
